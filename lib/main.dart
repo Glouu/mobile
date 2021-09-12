ed75e0d079c493351c9a10b0b22353acafacca85
Mensah Michael <msquaremega@gmail.com>
2021-09-12 05:26:16 +0100

    refresh redone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gloou/screens/splash/splash.dart';
 import 'package:gloou/shared/colors/colors.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +11,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Fellix',
-        primarySwatch: themeMainColor,
-        visualDensity: VisualDensity.adaptivePlatformDensity
+    return RefreshConfiguration(
+      headerBuilder: () => WaterDropHeader(),        // Configure the default header indicator. If you have the same header indicator for each page, you need to set this
+      footerBuilder:  () => ClassicFooter(),        // Configure default bottom indicator
+      headerTriggerDistance: 80.0,        // header trigger refresh trigger distance        // custom spring back animate,the props meaning see the flutter api
+      maxOverScrollExtent :100, //The maximum dragging range of the head. Set this property if a rush out of the view area occurs
+      maxUnderScrollExtent:0, // Maximum dragging range at the bottom
+      enableScrollWhenRefreshCompleted: true, //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
+      enableLoadingWhenFailed : true, //In the case of load failure, users can still trigger more loads by gesture pull-up.
+      hideFooterWhenNotFull: false, // Disable pull-up to load more functionality when Viewport is less than one screen
+      enableBallisticLoad: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Fellix',
+          primarySwatch: themeMainColor,
+          visualDensity: VisualDensity.adaptivePlatformDensity
+        ),
+        home: SplashView(),
       ),
-      home: SplashView(),
     );
   }
 }
--- lib/screens/general_home/general_home.dart
+++ lib/screens/general_home/general_home.dart
@@ -4,6 +4,7 @@ import 'package:gloou/screens/camera/camera_setup.dart';
 import 'package:gloou/screens/drawer/navigation_drawer.dart';
 import 'package:gloou/screens/feeds/feeds.dart';
 import 'package:gloou/shared/colors/colors.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class GeneralHome extends StatefulWidget {
   const GeneralHome({Key? key}) : super(key: key);
@@ -34,49 +35,57 @@ class _GeneralHomeState extends State<GeneralHome> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      drawer: _selectedIndex == 0 ? NavigationDrawer() : null,
-      appBar: getAppBar(),
-      body: SafeArea(
-        child: IndexedStack(
-          index: _selectedIndex,
-          children: pages,
-        ),
+    return RefreshConfiguration.copyAncestor(
+      context: context,
+      enableLoadingWhenFailed: true,
+      headerBuilder: () => WaterDropMaterialHeader(
+        backgroundColor: Theme.of(context).primaryColor,
       ),
-      bottomNavigationBar: _selectedIndex == 2
-          ? null
-          : BottomNavigationBar(
-              currentIndex: _selectedIndex,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.grey[500],
-              backgroundColor: Colors.black,
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: 'Search',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.camera_alt_sharp),
-                  label: 'Camera',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Profile',
-                ),
-              ],
-              onTap: (index) {
-                _selectedIndex = index;
-                setState(() {});
-              },
-            ),
+      footerTriggerDistance: 30,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        drawer: _selectedIndex == 0 ? NavigationDrawer() : null,
+        appBar: getAppBar(),
+        body: SafeArea(
+          child: IndexedStack(
+            index: _selectedIndex,
+            children: pages,
+          ),
+        ),
+        bottomNavigationBar: _selectedIndex == 2
+            ? null
+            : BottomNavigationBar(
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.grey[500],
+                backgroundColor: Colors.black,
+                type: BottomNavigationBarType.fixed,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Search',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.camera_alt_sharp),
+                    label: 'Camera',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Profile',
+                  ),
+                ],
+                onTap: (index) {
+                  _selectedIndex = index;
+                  setState(() {});
+                },
+              ),
+      ),
     );
   }
 
